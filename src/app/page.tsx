93bd1ad42661c6c7a890ff415e74bdb3899fbb5a
James Slater <thomas.james.slater@gmail.com>
2024-01-08 10:37:13 -0500

    homepage wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,59 @@
 "use client";
-import { Canvas, useFrame } from '@react-three/fiber';
-import { useRef, useState } from 'react';
-import { Mesh } from 'three';
-import { Container } from "@chakra-ui/react";
+import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber';
+import { Suspense, useRef, useState } from 'react';
+import { AmbientLight, Color, DirectionalLight, Mesh } from 'three';
+// @ts-ignore
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+
+const Model = () => {
+  const {height} = useThree((state) => state.viewport);
+  const fbx = useLoader(FBXLoader, "/objects/prefab_beech_tree_04.fbx")
+
+  return <primitive object={fbx} scale={.25} position={[0, -height * 25, -400]}/>;
+};
+
+function RotatingLights() {
+  const directionalLightRef = useRef<DirectionalLight>(null!);
+  const directionalLightRef2 = useRef<DirectionalLight>(null!);
+  let time = 0;
+
+  useFrame(() => {
+    if (directionalLightRef.current && directionalLightRef2.current) {
+      time += 0.01;
+
+      const radius = 1000;
+      const x = Math.sin(time) * radius;
+      const y = Math.cos(time) * radius;
+      directionalLightRef.current.intensity = 1
+      directionalLightRef.current.color = new Color(0, 55, 255)
+      directionalLightRef.current.position.set(x, y, 0);
+      directionalLightRef2.current.intensity = 1
+      directionalLightRef2.current.color = new Color(255, 55, 0)
+      directionalLightRef2.current.position.set(y, x, 0);
+    }
+  });
+
+  return (
+      <>
+        <directionalLight ref={directionalLightRef2} />
+        <directionalLight ref={directionalLightRef}/>
+      </>
+  );
+}
 
 export default function Home() {
+
+
   return (
       <div style={{width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
         <Canvas>
-          <ambientLight intensity={0.1}/>
-          <directionalLight color="white" position={[0, 0, 5]}/>
-          <SpinningMesh/>
+          {/*<ambientLight intensity={1.0} position={[0, 0, -5]}/>*/}
+          {/*<directionalLight color="white" position={[0, -10, -70]} intensity={1000}/>*/}
+          {/*<SpinningMesh/>*/}
+          <RotatingLights/>
+          <Suspense>
+            <Model/>
+          </Suspense>
         </Canvas>
       </div>
   );
