3630c3e60c32f4441511bb1d94242e326bf36b3f
James Slater <thomas.james.slater@gmail.com>
2024-01-09 11:09:47 -0500

    give it some flair

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,27 +5,40 @@ import { Heading } from "@chakra-ui/react";
 import { useState } from "react";
 
 export default function Home() {
-  const [loaded, setLoaded] = useState<boolean>(false)
+  const [loaded, setLoaded] = useState<boolean>(false);
   const onLoaded = () => {
-    setLoaded(true);
-  }
+    setTimeout(() => {
+      setLoaded(true);
+    }, 10)
+  };
   return (
-      <div
-          style={{
-            width: "100vw",
-            height: "100vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Heading
+        position={"absolute"}
+        fontSize={loaded ? "96px": "0"}
+        filter={"drop-shadow(0px 0px 6px)"}
+        transition={"all linear .4s"}
       >
-        <Heading position={"absolute"} fontSize={"128px"} filter={"drop-shadow(0px 0px 6px)"} display={loaded ? "": "none"}>
-          Slaterhaus
-        </Heading>
-        <Canvas>
-          <spotLight color="white" position={[0, 0, 20]} intensity={500}/>
-          {Array(10).fill(0).map((it, i) => <CubeGrid z={-i + 2} onLoaded={onLoaded} />)}
-        </Canvas>
-      </div>
+        Slaterhaus
+      </Heading>
+      <Canvas>
+        {/*<spotLight color="white" position={[0, 0, 20]} intensity={500}/>*/}
+        <spotLight color="white" position={[0, 0, 5]} intensity={50} />
+        <directionalLight color="blue" position={[0, 5, -5]} intensity={5} />
+        {Array(10)
+          .fill(0)
+          .map((it, i) => (
+            <CubeGrid z={-i + 2} onLoaded={onLoaded} />
+          ))}
+      </Canvas>
+    </div>
   );
 }
--- src/components/cube-grid.tsx
+++ src/components/cube-grid.tsx
@@ -2,62 +2,62 @@ import { useRef, useState } from "react";
 import { Color, Mesh, TextureLoader, Vector3 } from "three";
 import { useLoader } from "@react-three/fiber";
 
-export function Cube({position}: { position: Vector3 }) {
+export function Cube({ position }: { position: Vector3 }) {
   const mesh = useRef<Mesh>(null!);
-  const show = Math.round(Math.random());
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
 
   const [albedoMap, normalMap, roughnessMap, aoMap, heightMap] = useLoader(
-      TextureLoader,
-      [
-        "/objects/wall/TCom_Wall_Stone3_2x2_512_albedo.jpg",
-        "/objects/wall/TCom_Wall_Stone3_2x2_512_normal.jpg",
-        "/objects/wall/TCom_Wall_Stone3_2x2_512_roughness.jpg",
-        "/objects/wall/TCom_Wall_Stone3_2x2_512_ao.jpg",
-        "/objects/wall/TCom_Wall_Stone3_2x2_512_height.jpg",
-      ],
+    TextureLoader,
+    [
+      "/objects/wall/TCom_Wall_Stone3_2x2_512_albedo.jpg",
+      "/objects/wall/TCom_Wall_Stone3_2x2_512_normal.jpg",
+      "/objects/wall/TCom_Wall_Stone3_2x2_512_roughness.jpg",
+      "/objects/wall/TCom_Wall_Stone3_2x2_512_ao.jpg",
+      "/objects/wall/TCom_Wall_Stone3_2x2_512_height.jpg",
+    ],
   );
 
   // Create material with textures
   return (
-      <mesh
-          ref={mesh}
-          position={position}
-          scale={(hovered || !show) ? 0.0 : 1}
-          onClick={(event) => setActive(!active)}
-          onPointerOver={(event) => setHover(!hovered)}
-      >
-        <boxGeometry args={[1, 1, 1]}/>
-        <meshStandardMaterial
-            color={new Color(Math.random(), Math.random(), Math.random())}
-            displacementMap={heightMap}
-            aoMap={aoMap}
-            map={albedoMap}
-            normalMap={normalMap}
-            roughnessMap={roughnessMap}
-            displacementScale={0.0001}
-        />
-      </mesh>
+    <mesh
+      ref={mesh}
+      position={position}
+      scale={hovered ? 0.0 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(!hovered)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial
+        color={new Color(Math.random(), Math.random(), Math.random())}
+        displacementMap={heightMap}
+        aoMap={aoMap}
+        map={albedoMap}
+        normalMap={normalMap}
+        roughnessMap={roughnessMap}
+        displacementScale={0.0001}
+      />
+    </mesh>
   );
 }
 
-export function CubeGrid({z, onLoaded}: { z: number, onLoaded: () => void }) {
+export function CubeGrid({ z, onLoaded }: { z: number; onLoaded: () => void }) {
   const gridSize = 15;
   let cubes = [];
   for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
       // Position each cube in the grid
-      cubes.push(
+      if (j > 11 || j < 7) {
+        cubes.push(
           <Cube
-              key={`${i}-${j}`}
-              position={new Vector3(i - gridSize / 2, j - gridSize / 2, z)}
+            key={`${i}-${j}`}
+            position={new Vector3(i - gridSize / 2, j - gridSize / 2, z)}
           />,
-      );
+        );
+      }
     }
   }
   onLoaded();
-
 
   return <>{cubes}</>;
 }
