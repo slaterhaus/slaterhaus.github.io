efb53c058d07c5e52ab02bfb6244d50165ffc491
James Slater <thomas.james.slater@gmail.com>
2024-09-15 18:01:29 -0400

    type issue with mult-track-video page

--- src/app/multi-track-video/page.tsx
+++ src/app/multi-track-video/page.tsx
@@ -3,21 +3,20 @@ import React, {useCallback, useState} from 'react';
 import WebcamRecorder from "@/components/multi-track-video/webcam-recorder";
 import MultiTileScreen from "@/components/multi-track-video/multi-tile-screen";
 
-const Page = (callback: T, deps: React.DependencyList) => {
+const Page = () => {
     const [recordings, setRecordings] = useState<Blob[]>([]);
 
     const handleRecordingComplete = (recording: Blob) => {
-        console.log({recording})
         setRecordings([...recordings, recording]);
     };
     const reset = useCallback(() => {
         setRecordings([]);
-    }, deps)
+    }, [setRecordings])
 
     return (
         <div>
             <h1>Webcam Recording App</h1>
-            <button>Reset All</button>
+            <button onClick={reset}>Reset All</button>
             <WebcamRecorder onRecordingComplete={handleRecordingComplete} />
             <MultiTileScreen recordings={recordings} />
         </div>
